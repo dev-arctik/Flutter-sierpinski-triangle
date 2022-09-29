ad7fe754c45e97101166ad3fc35065345b7fb80f
Devansh <devansh.arctikcircle@gmail.com>
2022-09-29 18:21:13 +0530

    plotted the vertices

--- lib/widget/canvas.dart
+++ lib/widget/canvas.dart
@@ -1,18 +1,51 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class DrawingCanvas extends StatelessWidget {
   const DrawingCanvas({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(20),
-      child: SizedBox(
-        height: 200,
-        width: 500,
-        child: Card(
-          child: Text("Canvas area"),
-        ),
+    return Card(
+      child: CustomPaint(
+        //CustomPaint widget allows us to draw our custom shape. For more info check flutter docs
+        size: const Size(
+            double.infinity, double.infinity), //for max size as possible
+        painter: MyPainter(),
       ),
     );
   }
+}
+
+class MyPainter extends CustomPainter {
+  //here you add code to draw
+  @override
+  void paint(Canvas canvas, Size size) {
+    final dotPainter = Paint() //dotPainter is our Pen to draw
+      ..color = Colors.black //..color is equivalent to dotPainter.color
+      ..strokeWidth = 4
+      ..strokeCap = StrokeCap.round;
+
+    //drawing the vertices of triangle
+    const triangleVertices = PointMode.points;
+    double verticePadding =
+        size.width * 0.1; //to give some space from egde of canvas
+    List verticeCoordinates = [
+      [size.width / 2, verticePadding], //top vertix
+      [verticePadding, size.height - verticePadding], //left vertix
+      [
+        size.width - verticePadding,
+        size.height - verticePadding
+      ], //right vertix
+    ];
+    final drawVerticeCoordinates = [
+      Offset(verticeCoordinates[0][0], verticeCoordinates[0][1]),
+      Offset(verticeCoordinates[1][0], verticeCoordinates[1][1]),
+      Offset(verticeCoordinates[2][0], verticeCoordinates[2][1]),
+    ];
+    canvas.drawPoints(
+        triangleVertices, drawVerticeCoordinates, dotPainter); //plot vertices
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }
